cdd9dfaaca5e3b42cf93cd23fb3a4a2b3231cb4e
Jules Rubin <58521821+Pernam75@users.noreply.github.com>
2023-10-05 22:14:18 +0200

    Update resume link in AppBanner.jsx

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -51,7 +51,7 @@ const AppBanner = () => {
 				>
 					<a
 						download="RUBIN_Jules.pdf"
-						href="https://drive.google.com/uc?export=download&id=1HBwCYZyZWHwg9t-S2qCjg4TgPCNCS53e"
+						href="https://drive.google.com/uc?export=download&id=1UcZrgYIqQIB4mSKUJJTqzo8sX7V5O3uL"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
