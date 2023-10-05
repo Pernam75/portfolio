fde6a0649cace6a34c2e4e4b5d0b2c41e0ef0563
Jules Rubin <58521821+Pernam75@users.noreply.github.com>
2023-10-05 22:16:01 +0200

    Update aboutMeData.js

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,7 +1,7 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: "Hello, I'm Jules Rubin, a 21-year-old student currently in M1 pursuing a major in Data and Artificial Intelligence at EFREI Paris. I am a passionate individual with a strong interest in technology and sports. Over the years, I have cultivated my skills and knowledge in various areas, including web development, machine learning, and data analysis.",
+		bio: "Hello, I'm Jules Rubin, a 22-year-old student currently in M2 pursuing a major in Data and Artificial Intelligence at EFREI Paris. I am a passionate individual with a strong interest in technology and sports. Over the years, I have cultivated my skills and knowledge in various areas, including web development, machine learning, and data analysis.",
 	},
 	{
 		id: 2,
